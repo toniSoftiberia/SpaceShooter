a2f7901f67cec7ad3250399f44c8f4f4602cdab2
toniSoftiberia <alopezs1987+social@gmail.com>
2016-03-24 21:40:02 +0100

    Player Ship Changes color
    *Now Player ship & bolt chenges colors between orange & blue

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,13 +18,25 @@ public class PlayerController : MonoBehaviour {
     public Boundary bundary;
     public float fireRate = 0.25f;
 
-    public GameObject shot;
-    public Transform shotSpawn;
+    public GameObject blueShoot;
+    public GameObject orangeShoot;
+    public Transform shootSpawn;
 
 
     private Rigidbody _rb;
     private AudioSource _audio;
     private float _nextFire = 0.0f;
+
+    public enum ShipColor {
+        orange,
+        blue
+    };
+    
+    public ShipColor shipColor = ShipColor.orange;
+    
+    public Material orangeShip;
+    public Material blueShip;
+
 
     void Start()
     {
@@ -33,13 +45,17 @@ public class PlayerController : MonoBehaviour {
     }
 
 
-    void Update()
-    {
-        if (Input.GetButton("Fire1") && Time.time > _nextFire)
-        {
+    void Update(){
+        if (Input.GetButton("Fire1") && Time.time > _nextFire){
+            GameObject shoot = orangeShoot;
+            if (shipColor == ShipColor.blue)
+                shoot = blueShoot;
             _nextFire = Time.time + fireRate;
-            GameObject clone  = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+            GameObject clone  = Instantiate(shoot, shootSpawn.position, shootSpawn.rotation) as GameObject;
             _audio.Play();
+        }
+        if (Input.GetKeyDown("j")) {
+            ChangeShipColor();
         }
     }
 
@@ -60,5 +76,16 @@ public class PlayerController : MonoBehaviour {
            );
 
         _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
+    }
+
+    void ChangeShipColor() { 
+        if (shipColor == ShipColor.orange) {
+            GetComponent<Renderer>().sharedMaterial = blueShip;
+            shipColor = ShipColor.blue;
+        } else {
+            GetComponent<Renderer>().sharedMaterial = orangeShip;
+            shipColor = ShipColor.orange;
+        }
+
     }
 }
