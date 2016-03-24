efe8c6c865add4c9d318f86d10abf730ce13b81b
toniSoftiberia <alopezs1987+social@gmail.com>
2016-03-25 00:27:17 +0100

    *Changed  colors
    *Changed ship colors now using Purple & Red for all
    *Now you only can damage enemies of te diferent color

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,29 +6,27 @@ public class DestroyByContact : MonoBehaviour {
     public GameObject explosion;
     public GameObject playerExplosion;
     public int scoreValue = 50;
+    public string exception;
 
     private GameController gc;
 
-    void Start()
-    {
+    void Start() {
         GameObject gameControllerObject = GameObject.FindWithTag("GameController");
-        if (gameControllerObject != null)
-        {
+        if (gameControllerObject != null) {
             gc = gameControllerObject.GetComponent<GameController>();
         }
-        if (gc == null)
-        {
+        if (gc == null) {
             Debug.Log("Cannot find 'GameController' script");
         }
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if( other.tag != "Boundary")
-        {
+    void OnTriggerEnter(Collider other) {
+
+        if (other.tag == "BoltRed" || other.tag == "BoltPurple")
+            other.tag = other.tag;
+        if (other.tag != "Boundary" && (exception != null && other.tag != exception)) {
             Instantiate(explosion, transform.position, transform.rotation);
-            if (other.tag == "Player")
-            {
+            if (other.tag == "Player") {
                 Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
                 gc.GameOver();
             }
@@ -36,5 +34,7 @@ public class DestroyByContact : MonoBehaviour {
             Destroy(gameObject);
             gc.addScore(scoreValue);
         }
+        if (other.tag == exception)
+            Destroy(other.gameObject);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,11 @@
 using System.Collections;
 
 [System.Serializable]
-public class Boundary
-{
-    public float 
-        xMin = -6, 
-        xMax = 6, 
-        zMin = -4, 
+public class Boundary {
+    public float
+        xMin = -6,
+        xMax = 6,
+        zMin = -4,
         zMax = 8;
 }
 
@@ -18,8 +17,8 @@ public class PlayerController : MonoBehaviour {
     public Boundary bundary;
     public float fireRate = 0.25f;
 
-    public GameObject blueShoot;
-    public GameObject orangeShoot;
+    public GameObject purpleShoot;
+    public GameObject redShoot;
     public Transform shootSpawn;
 
 
@@ -28,30 +27,39 @@ public class PlayerController : MonoBehaviour {
     private float _nextFire = 0.0f;
 
     public enum ShipColor {
-        orange,
-        blue
+        red,
+        purple
     };
-    
-    public ShipColor shipColor = ShipColor.orange;
-    
-    public Material orangeShip;
-    public Material blueShip;
 
+    public ShipColor shipColor = ShipColor.red;
 
-    void Start()
-    {
+    public Material redShip;
+    public Material purpleShip;
+
+    public GameObject playerExplosion;
+
+    private GameController gc;
+
+    void Start() {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null) {
+            gc = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gc == null) {
+            Debug.Log("Cannot find 'GameController' script");
+        }
         _rb = GetComponent<Rigidbody>();
         _audio = GetComponent<AudioSource>();
     }
 
 
-    void Update(){
-        if (Input.GetButton("Fire1") && Time.time > _nextFire){
-            GameObject shoot = orangeShoot;
-            if (shipColor == ShipColor.blue)
-                shoot = blueShoot;
+    void Update() {
+        if (Input.GetButton("Fire1") && Time.time > _nextFire) {
+            GameObject shoot = redShoot;
+            if (shipColor == ShipColor.purple)
+                shoot = purpleShoot;
             _nextFire = Time.time + fireRate;
-            GameObject clone  = Instantiate(shoot, shootSpawn.position, shootSpawn.rotation) as GameObject;
+            GameObject clone = Instantiate(shoot, shootSpawn.position, shootSpawn.rotation) as GameObject;
             _audio.Play();
         }
         if (Input.GetKeyDown("j")) {
@@ -60,17 +68,16 @@ public class PlayerController : MonoBehaviour {
     }
 
 
-    void FixedUpdate ()
-    {
+    void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-       
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         _rb.velocity = (movement * speed);
 
         _rb.position = new Vector3(
-            Mathf.Clamp (_rb.position.x, bundary.xMin, bundary.xMax),
+            Mathf.Clamp(_rb.position.x, bundary.xMin, bundary.xMax),
             0.0f,
             Mathf.Clamp(_rb.position.z, bundary.zMin, bundary.zMax)
            );
@@ -78,14 +85,26 @@ public class PlayerController : MonoBehaviour {
         _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
     }
 
-    void ChangeShipColor() { 
-        if (shipColor == ShipColor.orange) {
-            GetComponent<Renderer>().sharedMaterial = blueShip;
-            shipColor = ShipColor.blue;
+    void ChangeShipColor() {
+        if (shipColor == ShipColor.red) {
+            GetComponent<Renderer>().sharedMaterial = purpleShip;
+            shipColor = ShipColor.purple;
         } else {
-            GetComponent<Renderer>().sharedMaterial = orangeShip;
-            shipColor = ShipColor.orange;
+            GetComponent<Renderer>().sharedMaterial = redShip;
+            shipColor = ShipColor.red;
         }
 
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag != "Boundary") {
+            if ((other.tag == "BoltRed" && shipColor == ShipColor.purple) ||
+                (other.tag == "BoltPurple" && shipColor == ShipColor.red)) {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+                gc.GameOver();
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+            } 
+        }
     }
 }
